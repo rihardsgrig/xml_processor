e19842746db53f19ffc987af08811f762b553663
Rihards Grigorjevs <rihards.grigorjevs@gmail.com>
2022-09-29 16:44:25 +0300

    skeleton

---
+++ src/Application.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Xml\Processor;
+
+use Monolog\Logger;
+use Symfony\Component\Console\Application as SymfonyApplication;
+use Symfony\Component\Console\Output\ConsoleOutput;
+use Symfony\Component\Console\Output\OutputInterface;
+
+final class Application
+{
+    private const APP_NAME = 'XML Processor';
+    private SymfonyApplication $app;
+    private OutputInterface $output;
+
+    public function __construct()
+    {
+        $this->output = new ConsoleOutput();
+
+        // @todo console logger and file logger
+//        $logger = new Logger(self::APP_NAME);
+//        $logger->pushHandler(
+//
+//        );
+
+        $this->app = new SymfonyApplication(
+            self::APP_NAME,
+            '@git@',
+        );
+    }
+
+    public function run(): void
+    {
+        $this->app->run(null, $this->output);
+    }
+}
